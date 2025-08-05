dd3c69d339359e5362035187f5dd91bbe6a58d2d
EZ3C <mboyer980@gmail.com>
2025-08-05 21:46:19 +0200

    Tensors can be reshaped

--- src/main.cpp
+++ src/main.cpp
@@ -27,33 +27,35 @@ int main()
     // tensorStatics();
 
     // Tensor::identity(2).print();
-    // Tensor test1({1, 2, 3, 4}, { 1, 2, 3, 4, 5, 6, 7, 8, 9,
-    // 10, 11, 12, 13, 14, 15, 16, 17, 18,
-    // 19, 20, 21, 22, 23, 24});
+    Tensor test1({1, 2, 3, 4}, { 1, 2, 3, 4, 5, 6, 7, 8, 9,
+    10, 11, 12, 13, 14, 15, 16, 17, 18,
+    19, 20, 21, 22, 23, 24});
 
+    test1.reshape({2 , 12}).print();
     // test1.print();
     // test1.mean().print();
-    // // .print();
-    // // std::cout << test1.mean() << "\n";
+    // .print();
+    // std::cout << test1.mean() << "\n";
 
-    // // Tensor test2({2, 3, 3, 2}, { 1, 2, 3, 4, 5, 6, 7, 8, 9,
-    // // 10, 11, 12, 13, 14, 15, 16, 17, 18,
-    // // 19, 20, 21, 22, 23, 24, 25, 26, 27,
-    // // 28, 29, 30, 31, 32, 33, 34, 35, 36});
+    // Tensor test2({2, 3, 3, 2}, { 1, 2, 3, 4, 5, 6, 7, 8, 9,
+    // 10, 11, 12, 13, 14, 15, 16, 17, 18,
+    // 19, 20, 21, 22, 23, 24, 25, 26, 27,
+    // 28, 29, 30, 31, 32, 33, 34, 35, 36});
 
-    // // for (size_t i = 0; i < 100000;i ++)
-    // //     test1.matmul(test2);
+    // for (size_t i = 0; i < 100000;i ++)
+    //     test1.matmul(test2);
 
-    // // (test + test).print();
-    // // (test - test).print();
-    // // (test * test).print();
-    // // (test / test).print();
+    // (test + test).print();
+    // (test - test).print();
+    // (test * test).print();
+    // (test / test).print();
 
 
-    // // (test + 1).print();
-    // // (test - 1).print();
-    // // (test * 2).print();
-    // // (test / 2).print();
+    // (test + 1).print();
+    // (test - 1).print();
+    // (test * 2).print();
+    // (test / 2).print();
+
 
 
     return 0;
